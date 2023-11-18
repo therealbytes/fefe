409b1ca4217f735aa9642202599a676d7bcb2ac8
bytes <therealbyt3s@gmail.com>
2023-11-19 01:49:52 +0300

    feat: fe implementation

--- src/FlashLoan.sol
+++ src/FlashLoan.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+interface IFlashLoanReceiver {
+    function executeOperation(
+        uint256 amount,
+        uint256 fee,
+        address initiator
+    ) external;
+}
+
 contract FlashLoan {
     receive() external payable {}
 
@@ -12,10 +20,15 @@ contract FlashLoan {
     constructor() {
         owner = msg.sender;
     }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "FlashLoan: Only owner");
+        _;
+    }
 
     function flashLoan(uint256 amount) external {
         uint256 balanceBefore = address(this).balance;
-        require(balanceBefore >= amount, "Insufficient liquidity");
+        require(balanceBefore >= amount, "FlashLoan: Insufficient liquidity");
         uint256 fee = (amount * feePerMillion) / 1e6;
         payable(msg.sender).transfer(amount);
 
@@ -27,26 +40,17 @@ contract FlashLoan {
 
         require(
             address(this).balance >= balanceBefore + fee,
-            "Loan not repaid with fee"
+            "FlashLoan: Loan not repaid with fee"
         );
         emit FlashLoanExecuted(msg.sender, amount);
     }
 
-    function setFeePerMillion(uint256 _feePerMillion) external {
-        require(msg.sender == owner, "Only owner");
+    function setFeePerMillion(uint256 _feePerMillion) onlyOwner external {
+        require(_feePerMillion <= 1e6, "FlashLoan: Fee too high");
         feePerMillion = _feePerMillion;
     }
 
-    function withdraw(address recipient, uint256 amount) external {
-        require(msg.sender == owner, "Only owner");
+    function withdraw(address recipient, uint256 amount) onlyOwner external {
         payable(recipient).transfer(amount);
     }
-}
-
-interface IFlashLoanReceiver {
-    function executeOperation(
-        uint256 amount,
-        uint256 fee,
-        address initiator
-    ) external;
 }
